79b8c062b315ef86400c13c3ea719a2c182abd03
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-25 15:43:30 +0700

    เมื่อคุณใช้คำสั่ง Docker CLI เพื่อสร้างคอนเทนเนอร์ใหม่

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 /* 
+เมื่อคุณใช้คำสั่ง Docker CLI เพื่อสร้างคอนเทนเนอร์ใหม่ เช่น `docker run -d --name ctr1 nginx` สิ่งที่เกิดขึ้นเบื้องหลังคือ:
 
-```
-$ docker rm web1 -f
-```
-// คำสั่งนี้ใช้สำหรับลบ Container ชื่อ "web1" ออกจากระบบ
-// -f หรือ --force เป็นการบังคับให้ลบ Container แม้ว่ามันจะยังคงรันอยู่
-
-```
-web1
-```
-// ผลลัพธ์แสดงชื่อ Container ที่ถูกลบออกไป
-
-```
-$ docker rmi test:latest
-```
-// คำสั่งนี้ใช้สำหรับลบ Docker Image ชื่อ "test" แท็ก "latest" ออกจากระบบ
-
-```
-Untagged: test:latest
-Deleted: sha256:0435f27...cac8e2b
-```
-
+1. Docker client แปลงคำสั่งเป็น API request และส่งไปยัง Docker daemon
+2. Docker daemon รับ request และส่งต่อไปยัง containerd เพื่อสร้างคอนเทนเนอร์
+3. containerd แปลง Docker image ที่ต้องการเป็น OCI bundle และบอก runc ให้สร้างคอนเทนเนอร์ใหม่
+4. runc เชื่อมต่อกับ OS kernel เพื่อรวบรวมส่วนประกอบที่จำเป็นในการสร้างคอนเทนเนอร์ (namespaces, cgroups เป็นต้น)
+5. คอนเทนเนอร์เริ่มต้นในฐานะ child process ของ runc และ runc จะออกไปเมื่อคอนเทนเนอร์เริ่มทำงาน
 */
